739338f73a1321e4d58c07755863bfffb16b2617
caleb-freitas <calebfreitas@tutanota.com>
2022-08-16 17:59:32 -0300

    feat: added cookie creation on initial request

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { nanoid } from 'nanoid'
+
+export function middleware(request: NextRequest) {
+  console.log(request.cookies)
+  // getting cookies from the request
+  const cookie = request.cookies.get("poll-token")
+
+  if (cookie) return
+
+  const response = NextResponse.next()
+
+  const cookieValue = nanoid()
+
+  // setting cookies on the response
+  response.cookies.set("poll-token", cookieValue, { sameSite: "strict" })
+
+  return response
+}
